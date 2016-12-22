1baa0f7c428c61571440b31e6ca59aa3b503ca15
Marko Stamenovic <markostam@gmail.com>
2016-12-21 19:41:02 -0500

    first two parts of exercise one done

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,6 +13,7 @@ object Main {
   /**
    * Exercise 1
    */
+   @annotation.tailrec
     def pascal(c: Int, r: Int): Int = {
       if (c == 0 | c == r) 1
       else{
@@ -23,7 +24,25 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = chars.foldLeft(0){
+      case (0, ')') => return false
+      case (x, '(') => x + 1
+      case (x, ')') => x - 1
+      case (x,  _ ) => x
+    } == 0
+
+    def balance2(chars: List[Char]): Boolean = {
+      def balancef(c: Int, chars2: List[Char]) : Int = {
+        (c, chars2) match {
+          case (0, ')'::_) => return 1
+          case (x, '('::t) => balancef(x + 1, t)
+          case (x, ')'::t) => balancef(x - 1, t)
+          case (x,  _ ::t) => balancef(x, t)
+          case (x, List()) => x
+        }
+      }
+      balancef(0, chars) == 0
+    }
   
   /**
    * Exercise 3
